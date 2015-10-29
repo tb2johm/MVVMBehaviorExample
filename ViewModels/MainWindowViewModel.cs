3570f7c833753648852f40617ebbdbebe136e96e
tb2johm <tb2johm@gmail.com>
2015-10-29 08:59:15 +0100

    Added base functionality, without behaviors

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using MMVVM.ViewModelBase;
+using MMVVM.Commands;
+
+namespace MVVMBehaviorExample.ViewModels
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        private string _text = String.Empty;
+        public string Text 
+        { 
+            get { return _text; }
+            set
+            {
+                if (value == null) return;
+                _text = value;
+                _textSaved = false;
+                Notify("Text");
+                SaveCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public RelayCommand SaveCommand { get; set; }
+
+        
+        
+        private bool _textSaved = true;
+
+
+
+        public MainWindowViewModel()
+        {
+            SaveCommand = new RelayCommand(() =>
+            {
+                _textSaved = true;
+                SaveCommand.RaiseCanExecuteChanged();
+            }, () => !_textSaved);
+        }
+    }
+}
